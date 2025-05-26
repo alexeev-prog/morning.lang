4aaa7f508fdc987b6025e614e72c9252e9e37bb4
Alexeev Bronislav <alexeev.dev@mail.ru>
2025-05-26 22:22:19 +0700

    add basic structure

---
+++ shell.nix
@@ -0,0 +1,56 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  libs = with pkgs; [
+    boost
+    cmake
+    clang
+    clang-tools
+    clang-tidy-sarif
+    clang-analyzer
+    libclang
+    llvm
+    stb
+    entt
+    gcc
+    lld
+    bolt
+    pkg-config
+    gdb
+    valgrind
+  ];
+  lib = libs;
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+      	jdinhlife.gruvbox
+        ms-vscode.cpptools
+        ms-vscode.cpptools-extension-pack
+        llvm-vs-code-extensions.vscode-clangd
+      ];
+    })
+    cppcheck
+    clang-tools
+    codespell
+    conan
+    doxygen
+    gtest
+    lcov
+    vcpkg
+    vcpkg-tool
+  ];
+  buildInputs = libs;
+  # XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
+  # XDG_RUNTIME_DIRS = "/run/user/1001/";
+  shellHook = ''
+    echo "morning.lang Dev Shell"
+    # Ensure proper environment setup for GCC and glibc
+    # Correct the CXXFLAGS by removing `.dev` from the GCC path
+    export CXXFLAGS="-I${pkgs.gcc}/include/c++/${pkgs.gcc.version} -I${pkgs.glibc}/include"
+
+    export CC=clang
+    export CXX=clang++
+  '';
+}
+
